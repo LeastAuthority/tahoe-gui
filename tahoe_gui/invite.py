7f026a402d9ae573b1fd41b14120b3948736ff6b
Chris Wood <chris@leastauthority.com>
2016-11-16 08:30:44 -0500

    Extend setup to create magic-folder in ~/Private

--- tahoe_gui/invite.py
+++ tahoe_gui/invite.py
@@ -1,19 +1,22 @@
 # -*- coding: utf-8 -*-
 
+import binascii
 import json
+import os
 
 from PyQt5.QtCore import Qt, QStringListModel
 from PyQt5.QtGui import QFont, QPixmap
 from PyQt5.QtWidgets import (
-    QCheckBox, QCompleter, QHBoxLayout, QLabel, QLineEdit, QMessageBox,
-    QProgressBar, QSizePolicy, QSpacerItem, QVBoxLayout, QWidget)
+    QCheckBox, QCompleter, QHBoxLayout, QLabel, QLineEdit, QProgressBar,
+    QSizePolicy, QSpacerItem, QVBoxLayout, QWidget)
 from twisted.internet import reactor
-from twisted.internet.defer import inlineCallbacks, returnValue
+from twisted.internet.defer import Deferred, inlineCallbacks, returnValue
 from wormhole.wordlist import raw_words
 from wormhole.wormhole import wormhole
 
+from tahoe_gui.config import config_dir
 from tahoe_gui.resource import resource
-
+from tahoe_gui.tahoe import Tahoe
 
 appid = u"lothar.com/wormhole/text-or-file-xfer"
 relay = u"ws://wormhole-relay.lothar.com:4000/v1"
@@ -39,6 +42,12 @@ def is_valid(code):
         return True
 
 
+def sleep(seconds):
+    d = Deferred()
+    reactor.callLater(seconds, d.callback, seconds)
+    return d
+
+
 # Adapted from 'xfer_util.py' which is not yet in magic-wormhole stable
 # https://github.com/warner/magic-wormhole/blob/master/src/wormhole/xfer_util.py
 @inlineCallbacks
@@ -53,7 +62,6 @@ def wormhole_receive(code, use_tor=None):
     msg = None
     if 'message' in offer:
         msg = offer['message']
-        print(msg)
         wh.send(json.dumps({"answer": {"message_ack": "ok"}}).encode("utf-8"))
     else:
         raise Exception("Unknown offer type: {}".format(offer.keys()))
@@ -181,22 +189,55 @@ class InviteForm(QWidget):
 
     @inlineCallbacks
     def setup(self, code):
-        self.update_progress(10, 'Opening wormhole...')
-        settings = yield wormhole_receive(code)
-        self.update_progress(20, 'Configuring gateway...')
-        returnValue(settings)
-        # TODO:
-        # run 'tahoe create-client /path/to/configdir/id'
-        # write settings to tahoe.cfg
-        # run 'tahoe -d /path/to/configdir/id start'
-        # wait on connection (3 secs? Poll welcome page?)
-        # create magic-folder in ~/Private
-        # run 'tahoe -d /path/to/configdir/id restart'
-        # open ~/Private folder (via xdg-open, etc.)
+        folder = os.path.join(os.path.expanduser('~'), 'Private')
+        try:
+            os.makedirs(folder)
+        except OSError:
+            pass
+        self.update_progress(12.5, 'Opening wormhole...')
+        data = yield wormhole_receive(code)
+        settings = json.loads(data)
+
+        self.update_progress(25, 'Creating gateway...')
+        nodeid = binascii.b2a_hex(os.urandom(32)).decode('utf-8')
+        tahoe = Tahoe(os.path.join(config_dir, nodeid))
+        args = ['create-client', '--webport=tcp:0:interface=127.0.0.1']
+        for option in ('nickname', 'introducer'):
+            # TODO: Add 'needed', 'happy', 'total' pending tahoe-lafs PR #376
+            # https://github.com/tahoe-lafs/tahoe-lafs/pull/376
+            if option in settings:
+                args.extend(['--{}'.format(option), settings[option]])
+        yield tahoe.command(args)
+
+        self.update_progress(37.5, 'Configuring gateway...')
+        for option in ('needed', 'happy', 'total'):
+            if option in settings:
+                tahoe.config_set('client', 'shares.{}'.format(option),
+                                 settings[option])
+
+        self.update_progress(50, 'Starting gateway...')
+        yield tahoe.start()
+
+        self.update_progress(62.5, 'Connecting to grid...')
+        # TODO: Replace with call to "readiness" API?
+        # https://tahoe-lafs.org/trac/tahoe-lafs/ticket/2844
+        yield sleep(2)
+
+        self.update_progress(75, 'Creating magic-folder...')
+        yield tahoe.command(['magic-folder', 'create', 'magic:', 'admin',
+                             folder])
+
+        self.update_progress(87.5, 'Reloading...')
+        yield tahoe.start()
+
+        self.update_progress(100, 'Done!')
+        yield sleep(1)
+        # TODO: Open local folder with file manager instead?
+        yield tahoe.command(['webopen'])
+        returnValue(None)
 
     def done(self, msg):
-        QMessageBox.information(
-            self, "Received response:", msg, QMessageBox.Ok)
+        print(msg)
         self.close()
 
     def return_pressed(self):
